2be4028eacfd57ed7e1cce6ac91836af5139e85b
sjpark <tjdwns9656@naver.com>
2023-02-20 11:54:00 +0900

    Triangle 클래스화

--- CGameEdu01.cpp
+++ CGameEdu01.cpp
@@ -8,28 +8,16 @@ CGameEdu01::~CGameEdu01(void)
 {
 }
 
+
+
 void CGameEdu01::OnInit()
 {
-	CUSTOMVERTEX vertices[] =
-	{
-		{ 150.0f, 50.0f, 0.5f, 1.0f, 0xffff0000, },
-		{ 250.0f, 250.0f, 0.5f, 1.0f, 0xff00ff00, },
-		{ 50.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff, },
-	};
-
-	m_pd3dDevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &m_pVB, NULL);
-
-	VOID* pVertices;
-	m_pVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0);
-	memcpy(pVertices, vertices, sizeof(vertices));
-	m_pVB->Unlock();
+	m_Triangle.OnInit(m_pd3dDevice);
 }
 
 void CGameEdu01::OnRender()
 {
-	m_pd3dDevice->SetStreamSource(0, m_pVB, 0, sizeof(CUSTOMVERTEX));
-	m_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-	m_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+	m_Triangle.OnRender();
 }
 
 void CGameEdu01::OnUpdate()
@@ -38,7 +26,6 @@ void CGameEdu01::OnUpdate()
 
 void CGameEdu01::OnRelease()
 {
-	if (m_pVB != NULL)
-		m_pVB->Release();
+	m_Triangle.OnRelease();
 }
 
--- CGameEdu01.h
+++ CGameEdu01.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "CD3DApp.h"
+#include "CTriangle.h"
 
-struct CUSTOMVERTEX
-{
-	FLOAT x, y, z, rhw; // The transformed position for the vertex.
-	DWORD color;		// The vertex color.
-};
+//struct CUSTOMVERTEX
+//{
+//	FLOAT x, y, z, rhw; // The transformed position for the vertex.
+//	DWORD color;		// The vertex color.
+//};
 
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW|D3DFVF_DIFFUSE)
+//#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW|D3DFVF_DIFFUSE)
 
 class CGameEdu01 : public CD3DApp
 {
 private:
-	LPDIRECT3DVERTEXBUFFER9 m_pVB;
+	CTriangle m_Triangle;
+	/*LPDIRECT3DVERTEXBUFFER9 m_pVB;*/
 
 private:
 	virtual void OnInit();
